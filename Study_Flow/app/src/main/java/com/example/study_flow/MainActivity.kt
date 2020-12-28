6ffa989c7238cb79f9f7a9ccebeac1f9a5e776a5
liurui <liurui@yixin.im>
2020-12-28 16:49:23 +0800

    flow添加相关注释

--- Study_Flow/app/src/main/java/com/example/study_flow/MainActivity.kt
+++ Study_Flow/app/src/main/java/com/example/study_flow/MainActivity.kt
@@ -12,6 +12,18 @@ import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.onEach
 
+/**
+ * 使用纯Flow作为LiveData替代品的主要问题是：
+
+1.Flow是无状态的（并且不能通过.value访问）。
+2.Flow是声明性的,一个Flow Builder仅描述Flow是什么，并且仅在收集时才具体化。 并且将为每个收集器有效地实例化新的Flow，这意味着将为每个收集器冗余且重复地运行上游昂贵的数据库访问。
+3.Flow本身对Android生命周期一无所知，并且不会在Android生命周期状态发生变化时自动暂停和恢复收集器
+
+作者：RicardoMJiang
+链接：https://juejin.cn/post/6906465243564933128
+来源：掘金
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+ */
 class MainActivity : AppCompatActivity(), CoroutineScope by MainScope() {
     //需要activity-ktx的支持
     private val roomViewModel: RoomViewModel by viewModels()
