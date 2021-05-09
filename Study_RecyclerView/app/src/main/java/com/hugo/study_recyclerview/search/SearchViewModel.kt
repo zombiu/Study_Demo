fa1dc783468e2f51a82c55179d6040edb66bfcec
zomubi <zomubi@163.com>
2021-05-09 18:27:45 +0800

    rx操作符添加注释

--- Study_RecyclerView/app/src/main/java/com/hugo/study_recyclerview/search/SearchViewModel.kt
+++ Study_RecyclerView/app/src/main/java/com/hugo/study_recyclerview/search/SearchViewModel.kt
@@ -1,6 +1,7 @@
 package com.hugo.study_recyclerview.search
 
 import android.app.Application
+import android.util.SparseArray
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import com.blankj.utilcode.util.LogUtils
@@ -22,32 +23,33 @@ class SearchViewModel(application: Application) : AndroidViewModel(application) 
         disposable = keywordLiveData.toReactiveStream()
             // 在filter中返回true表示发射该元素，返回false表示过滤该数据。
             .filter {
+                LogUtils.e("-->>当前线程")
                 if (it.isNullOrBlank()) {
-                    //为空时，清空搜索结果
+                    //为空时，要立即清空搜索结果
                 }
                 true
             }
-            // 去抖动
+            // 去抖动,只发送最新的数据 下游运行在子线程中
             .debounce(1000, TimeUnit.MILLISECONDS)
             /*.debounce {
                 //连续的输入，时间间隔小于300ms，将不会触发搜索操作
                 Flowable.timer(1000, TimeUnit.MILLISECONDS)
             }*/
             .filter {
+                LogUtils.e("-->>当前线程")
                 it.isNotBlank()
             }
-            // 去掉数据源重复的数据,直到数据不跟上一个数据重复 例如 1, 1, 2, 1, 2, 3, 3, 4 打印：1 2 1 2 3 4
+            // 去掉数据源连续重复的数据,直到数据不跟上一个数据重复 例如 1, 1, 2, 1, 2, 3, 3, 4 打印：1 2 1 2 3 4
             .distinctUntilChanged()
+                // switchMap操作符只发射最近的Observables，取消旧订阅
             .switchMap {
+                LogUtils.e("-->>当前线程")
                 //调用接口进行搜索
                 Flowable.just(it)
             }
             .doOnNext {
-                //处理搜索接口，并可根据接口返回，控制间隔时间
-                /*if (timeOut != it.call_interval) {
-                    timeOut = it.call_interval
-                }*/
                 LogUtils.e("-->>最终结果$it<<  ${it == null}")
+                searchResultLiveData.postValue(it)
             }
 //            .autoDisposable(this)
             .subscribe()
