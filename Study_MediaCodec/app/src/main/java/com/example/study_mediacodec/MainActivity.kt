74aee5d2b81a87cf7731840782c710d9047902c7
liurui <303996789@qq.com>
2022-03-23 20:27:00 +0800

    解码播放音频

--- Study_MediaCodec/app/src/main/java/com/example/study_mediacodec/MainActivity.kt
+++ Study_MediaCodec/app/src/main/java/com/example/study_mediacodec/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.study_mediacodec.databinding.ActivityMainBinding
+import com.example.study_mediacodec.video.AudioActivity
 import com.example.study_mediacodec.video.VideoActivity
 
 class MainActivity : AppCompatActivity() {
@@ -15,6 +16,10 @@ class MainActivity : AppCompatActivity() {
 
         binding.tv1.setOnClickListener {
             startActivity(Intent(this, VideoActivity::class.java))
+        }
+
+        binding.tv1.setOnClickListener {
+            startActivity(Intent(this, AudioActivity::class.java))
         }
     }
 }
--- Study_MediaCodec/app/src/main/java/com/example/study_mediacodec/video/AudioDecoder.java
+++ Study_MediaCodec/app/src/main/java/com/example/study_mediacodec/video/AudioDecoder.java
@@ -1,5 +1,8 @@
 package com.example.study_mediacodec.video;
 
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioTrack;
 import android.media.MediaCodec;
 import android.media.MediaCodecList;
 import android.media.MediaExtractor;
@@ -10,53 +13,81 @@ import android.view.Surface;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 
-public class VideoDecoder implements Runnable {
-    private static final String TAG = "-->>" + VideoDecoder.class.getName();
+/**
+ *
+ */
+public class AudioDecoder implements Runnable {
+    private final String TAG = "-->>" + this.getClass().getName();
+
+    private static String VIDEO = "video/";
+    private static String AUDIO = "audio/";
 
     private MediaExtractor extractor;
-    private MediaCodec codec;
+    private static MediaCodec decoder;
+    private MediaFormat mediaFormat;
     private boolean isEOS;
-    private Surface surface;
     private static long TIME_OUT_US = 1000L;
 
-    public void init(Surface surface, String videoPath) {
-        this.surface = surface;
+    public void init(String path) {
         extractor = new MediaExtractor();
         try {
-            extractor.setDataSource(videoPath);
-            MediaFormat mediaFormat = chooseVideoTrack(extractor);
-            codec = createCodec(mediaFormat, surface);
+            extractor.setDataSource(path);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+        mediaFormat = chooseVideoTrack(extractor, AUDIO);
+        if (mediaFormat == null) {
+            return;
+        }
+        initAudioParams(mediaFormat);
+        try {
+            decoder = createCodec(mediaFormat, null);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    private void initAudioParams(MediaFormat mediaFormat) {
+
+    }
+
     @Override
     public void run() {
-        codec.start();
+        decoder.start();
+        MediaCodec.BufferInfo bufferInfo = new MediaCodec.BufferInfo();
+        int sampleRate = mediaFormat.getInteger(MediaFormat.KEY_SAMPLE_RATE);
+        int channelCount = mediaFormat.getInteger(MediaFormat.KEY_CHANNEL_COUNT);
+
+        int bufferSize = AudioTrack.getMinBufferSize(sampleRate, AudioFormat.CHANNEL_OUT_STEREO, AudioFormat.ENCODING_PCM_16BIT);
+        AudioTrack audioTrack = new AudioTrack(
+                AudioManager.STREAM_MUSIC,
+                sampleRate, AudioFormat.CHANNEL_OUT_STEREO,
+                AudioFormat.ENCODING_PCM_16BIT, bufferSize, AudioTrack.MODE_STREAM);
+        audioTrack.play();
+
         long startTime = 0l;
         boolean first = false;
         while (!isEOS) {
-            int inputBufferId = codec.dequeueInputBuffer(0);
+            int inputBufferId = decoder.dequeueInputBuffer(0);
             if (inputBufferId >= 0) {
-                ByteBuffer buffer = codec.getInputBuffer(inputBufferId);
+                ByteBuffer buffer = decoder.getInputBuffer(inputBufferId);
                 int sampleSize = extractor.readSampleData(buffer, 0);
                 if (sampleSize < 0) {
-                    codec.queueInputBuffer(inputBufferId, 0, 0, 0, MediaCodec.BUFFER_FLAG_END_OF_STREAM);
+                    decoder.queueInputBuffer(inputBufferId, 0, 0, 0, MediaCodec.BUFFER_FLAG_END_OF_STREAM);
                     isEOS = true;
                 } else {
                     long sampleTime = extractor.getSampleTime();
-                    codec.queueInputBuffer(inputBufferId, 0, sampleSize, sampleTime, 0);
+                    decoder.queueInputBuffer(inputBufferId, 0, sampleSize, sampleTime, 0);
                     extractor.advance();
                 }
             }
 
-            MediaCodec.BufferInfo bufferInfo = new MediaCodec.BufferInfo();
-            int outIndex = codec.dequeueOutputBuffer(bufferInfo, TIME_OUT_US);
+            int outIndex = decoder.dequeueOutputBuffer(bufferInfo, TIME_OUT_US);
             Log.i(TAG, "run: outIndex=" + outIndex);
             switch (outIndex) {
                 case MediaCodec.INFO_OUTPUT_FORMAT_CHANGED:
-                    Log.i(TAG, "run: new format: " + codec.getOutputFormat());
+                    Log.i(TAG, "run: new format: " + decoder.getOutputFormat());
                     break;
                 case MediaCodec.INFO_TRY_AGAIN_LATER:
                     Log.i(TAG, "run: try later");
@@ -70,6 +101,11 @@ public class VideoDecoder implements Runnable {
                         startTime = System.currentTimeMillis();
                     }
 
+                    ByteBuffer outputBuffer = decoder.getOutputBuffer(outIndex);
+                    byte[] chunk = new byte[bufferInfo.size];
+                    // 将解码后的音频数据 拷贝到 chunk中
+                    outputBuffer.get(chunk);
+
                     // PTS全称：Presentation Time Stamp(以微秒为单位)。用于标示解码后的视频帧什么时候被显示出来。 在没有B帧的情况下，DTS和PTS的输出顺序是一样的，一旦存在B帧，PTS和DTS则会不同。
                     // 如果解码过快，这里控制一下播放速度，跟pts保持一致
                     long sleepTime = bufferInfo.presentationTimeUs / 1000 - (System.currentTimeMillis() - startTime);
@@ -80,7 +116,8 @@ public class VideoDecoder implements Runnable {
                             e.printStackTrace();
                         }
                     }
-                    codec.releaseOutputBuffer(outIndex, true);
+                    audioTrack.write(chunk, bufferInfo.offset, bufferInfo.offset + bufferInfo.size);
+                    decoder.releaseOutputBuffer(outIndex, true);
                     break;
                 }
             }
@@ -93,13 +130,12 @@ public class VideoDecoder implements Runnable {
         }
     }
 
-    private MediaFormat chooseVideoTrack(MediaExtractor extractor) {
+    private MediaFormat chooseVideoTrack(MediaExtractor extractor, String mimePrefix) {
         int count = extractor.getTrackCount();
         for (int i = 0; i < count; i++) {
             MediaFormat format = extractor.getTrackFormat(i);
-            if (format.getString(MediaFormat.KEY_MIME).startsWith("video/")) {
-                //选择轨道
-                extractor.selectTrack(i);
+            if (format.getString(MediaFormat.KEY_MIME).startsWith(mimePrefix)) {
+                extractor.selectTrack(i);//选择轨道
                 return format;
             }
         }
@@ -107,6 +143,7 @@ public class VideoDecoder implements Runnable {
     }
 
     private MediaCodec createCodec(MediaFormat format, Surface surface) throws IOException {
+        // 用于 MediaCodecList(int)仅枚举适用于常规（缓冲区到缓冲区）解码或编码的编解码器。
         MediaCodecList codecList = new MediaCodecList(MediaCodecList.REGULAR_CODECS);
         MediaCodec codec = MediaCodec.createByCodecName(codecList.findDecoderForFormat(format));
         codec.configure(format, surface, null, 0);
