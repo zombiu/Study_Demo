7a1d8b81066dbe72daa645bb1c8eea4c7ac166c3
zomubi <303996789@qq.com>
2022-03-26 15:06:24 +0800

    修复多点触控崩溃bug，添加注释

--- Study_TouchEvent/app/src/main/java/com/example/study_touchevent/MyTouchView2.java
+++ Study_TouchEvent/app/src/main/java/com/example/study_touchevent/MyTouchView2.java
@@ -69,7 +69,9 @@ public class MyTouchView2 extends View {
         int actionMasked = event.getActionMasked();
         switch (actionMasked) {
             case MotionEvent.ACTION_DOWN: {
-                Log.e("-->>", "ACTION_DOWN");
+                // 重置一下 某些情况 ACTION_DOWN 发生在 ACTION_POINTER_DOWN之后
+                actionIndex = 0;
+                Log.e("-->>", "ACTION_DOWN " + event.getActionIndex());
                 downX = (int) event.getX();
                 downY = (int) event.getY();
                 // 需要返回true 告诉父view  我要消费这个事件
@@ -106,33 +108,45 @@ public class MyTouchView2 extends View {
                 break;
             }
             case MotionEvent.ACTION_POINTER_UP: {
+                // 某些事件 可能连续触发 比如 ACTION_POINTER_UP 还需要兼容
                 Log.e("-->>", "ACTION_POINTER_UP getActionIndex=" + event.getActionIndex());
                 // 获取 对应索引的id
 //                int pointerId = event.getPointerId(actionIndex);
 //                int pointerIndex = event.findPointerIndex(pointerId);
 
-                //  当0号索引的手指 从屏幕上抬起时， 触发 ACTION_POINTER_UP actionIndex需要-1
-                if (event.getActionIndex() == 0) {
-                    if (event.getActionIndex() == actionIndex) {
-                        actionIndex = event.getPointerCount() - 2;
+                int oldActionIndex = actionIndex;
 
-                        // 注意 这里需要重置一下 偏移量到 新手指上
-                        downX = (int) event.getX(actionIndex);
-                        downY = (int) event.getY(actionIndex);
+                //  当0号索引的手指 从屏幕上抬起时， 触发 ACTION_POINTER_UP actionIndex需要-1
+
+                // 判断逻辑
+                // 1.如果抬起的是 当前抢夺事件的手指
+                if (actionIndex == event.getActionIndex()) {
+                    // a.如果是0号手指，将事件交给 event.getPointerCount() - 2号索引的 手指处理
+                    if (actionIndex == 0) {
+                        actionIndex = event.getPointerCount() - 2;
+                        Log.e("-->>", "POINTER_UP 1");
                     } else {
+                        // b.如果不是0号手指，那么必定时索引最大的手指，此时-1即可。
                         actionIndex = actionIndex - 1;
+                        Log.e("-->>", "POINTER_UP 2");
                     }
                 } else {
-                    if (event.getActionIndex() == actionIndex) {
+                    // 2.如果抬起的不是 当前抢夺事件的手指
+                    // a.如果抬起的是0号手指， 那么此时抢夺事件的手指必定是索引最大的手指，那么索引-1即可
+                    if (event.getActionIndex() == 0) {
                         actionIndex = actionIndex - 1;
-
-                        // 注意 这里需要重置一下 偏移量到 新手指上
-                        downX = (int) event.getX(actionIndex);
-                        downY = (int) event.getY(actionIndex);
+                        Log.e("-->>", "POINTER_UP 3");
                     } else {
-                        // 当actionIndex = 0 并且 抬起的不是0号索引手指时， 什么也不做
-                        Log.e("-->>", "什么也不做");
+                        // b.如果抬起的不是0号手指，那么抬起的必定是索引最大的手指，，将事件交给 event.getPointerCount() - 2 号索引手指处理即可
+                        actionIndex = event.getPointerCount() - 2;
+                        Log.e("-->>", "POINTER_UP 4");
                     }
+                }
+
+                // 注意 这里需要重置一下 偏移量到 新手指上
+                if (oldActionIndex == event.getActionIndex()) {
+                    downX = (int) event.getX(actionIndex);
+                    downY = (int) event.getY(actionIndex);
                 }
 
                 Log.e("-->>", "ACTION_POINTER_UP 之后处理事件的索引=" + actionIndex);
