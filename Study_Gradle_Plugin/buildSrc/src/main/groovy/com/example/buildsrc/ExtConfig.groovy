a2e0d687721a3c2750a32aa9751f01c5054b16df
liurui <303996789@qq.com>
2023-08-10 16:40:28 +0800

    ext额外属性

--- Study_Gradle_Plugin/buildSrc/src/main/groovy/com/example/buildsrc/ExtConfig.groovy
+++ Study_Gradle_Plugin/buildSrc/src/main/groovy/com/example/buildsrc/ExtConfig.groovy
@@ -2,4 +2,9 @@ package com.example.buildsrc;
 
 class ExtConfig {
     String message = "this is old value"
+    int count = 0
+
+    void count(int count) {
+        this.count = count
+    }
 }
--- Study_Gradle_Plugin/buildSrc/src/main/groovy/com/example/buildsrc/MyTestPlugin.groovy
+++ Study_Gradle_Plugin/buildSrc/src/main/groovy/com/example/buildsrc/MyTestPlugin.groovy
@@ -10,13 +10,13 @@ class MyTestPlugin implements Plugin<Project> {
         println("custom plugin -->>this is a plugin in the file 'build.gradle'...")
         //定义 扩展，并且与 ExtConfig 进行绑定。
         ExtConfig extConfig = project.extensions.create('extConfig', ExtConfig.class)
-        // 创建了一个 task  printExtConfig
+        // 创建了一个 task  printExtConfig   执行对应task 使用命令 gradlew printExtConfig  后面是task名
         Task task = project.tasks.create('printExtConfig') {
             //通过 extConfig 属性，获取外部传递进来的 message 的具体内容。
             println("DemoPlugin message =" + project.extConfig.message)
             // task 执行完后 会去执行 doLast 这里可以拿到最新的 extension设置的值
             doLast {
-                println("custom plugin -->> doLast message=${extConfig.message}")
+                println("custom plugin -->> doLast message=${extConfig.message} count=${extConfig.count}")
             }
 
         }
